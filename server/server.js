58bd3235d9051b868d0817385fb5b9b023adfaaa
Nikos Argalias <nik.argalias@gmail.com>
2021-10-12 16:08:20 +0100

    configure heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const port = process.send.PORT || 3000;
 
 const publicPath = path.join(__dirname, '..', 'dist');
 
@@ -10,6 +11,6 @@ app.get('*', (request, response) => {
     response.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up');
 });
